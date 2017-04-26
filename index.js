bfdca01685e2d7be21c639c273df187926f696a3
anmatika <4ntti.matikainen@gmail.com>
2017-04-26 21:32:42 +0300

    refac

--- index.js
+++ index.js
@@ -13,18 +13,6 @@ const msg = process.argv[2];
 const output = require('./outputter');
 const aes192 = require('./aes192');
 
-if (msg === undefined){
-    console.log(chalk.red('ERROR: Please give the msg.'));
-    process.exit(1);
-}
-
-run();
-
-function run() {
-    const opts = getCommandlineOptions();
-    outputHashes(msg, opts);
-}
-
 function getCommandlineOptions() {
     const options = [
         { name: 'encrypt', type: 'bool' },
@@ -34,6 +22,7 @@ function getCommandlineOptions() {
     const opts = dashdash.parse({options: options});
     return opts;
 }
+
 function outputHashes(text, opts){
     debugger;
     if (opts.decrypt){
@@ -44,3 +33,12 @@ function outputHashes(text, opts){
     output.outputEncryptedAes192(text, opts.salt);
     return;
 }
+
+if (msg === undefined){
+  console.log(chalk.red('ERROR: Please give the msg.'));
+  process.exit(1);
+
+}
+
+const opts = getCommandlineOptions();
+outputHashes(msg, opts);
